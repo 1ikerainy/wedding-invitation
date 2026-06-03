49c829e040efbe29006d8fb13772c6768c990fc7
likerainy <minerba89@gmail.com>
2026-06-03 21:08:37 +0900

    사진 변경

--- src/images/index.ts
+++ src/images/index.ts
@@ -17,6 +17,23 @@ import image15 from "./image15.jpg"
 import image16 from "./image16.jpg"
 import image17 from "./image17.jpg"
 import image18 from "./image18.jpg"
+import image19 from "./image19.jpg"
+import image20 from "./image20.jpg"
+import image21 from "./image21.jpg"
+import image22 from "./image22.jpg"
+import image23 from "./image23.jpg"
+import image24 from "./image24.jpg"
+import image25 from "./image25.jpg"
+import image26 from "./image26.jpg"
+import image27 from "./image27.jpg"
+import image28 from "./image28.jpg"
+import image29 from "./image29.jpg"
+import image30 from "./image30.jpg"
+import image31 from "./image31.jpg"
+import image32 from "./image32.jpg"
+import image33 from "./image33.jpg"
+import image34 from "./image34.jpg"
+import image35 from "./image35.jpg"
 
 export const COVER_IMAGE = coverImage
 
@@ -38,5 +55,22 @@ export const GALLERY_IMAGES = [
   image15,
   image16,
   image17,
-  image18
+  image18,
+  image19,
+  image20,
+  image21,
+  image22,
+  image23,
+  image24,
+  image25,
+  image26,
+  image27,
+  image28,
+  image29,
+  image30,
+  image31,
+  image32,
+  image33,
+  image34,
+  image35
 ]
